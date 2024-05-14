1f09495dd5437c5e930f517149714e1cf1cc8c16
Vladislav Shelepkov <shelepkov.vladislav@gmail.com>
2024-05-14 12:23:11 +0200

    inited

--- content.js
+++ content.js
@@ -1,21 +1,33 @@
-if (window.location.host === "ston.fi") {
-    alert("hi");
-    const currentTime = new Date().toISOString();
-    chrome.storage.local.set({"lastVisited": currentTime}, function() {
-        console.log("The time has been saved: " + currentTime);
-    });
-}
+window.addEventListener('load', function () {
+    setTimeout(() => {
+        if (window.location.host === "app.ston.fi") {
+            const currentTime = new Date().toISOString();
+            chrome.storage.local.set({"lastVisited": currentTime}, function () {
+                console.log("The time has been saved: " + currentTime);
+            });
+        }
 
-if (window.location.pathname.startsWith('/pools/')) {
-    const header = document.querySelector('header._pool-overview_2m9pv_1.grid.gap-x-7.xl\\:grid-cols-\\[640px_auto\\]');
+        if (new RegExp("/pools/[^/]+").test(window.location.pathname)) {
+            const mainElement = document.querySelector('main.flex-1');
+            if (mainElement) {
+                const header = mainElement.querySelector('header');
+                if (header) {
+                    const newBlock = document.createElement('div');
+                    newBlock.id = 'graph';
+                    newBlock.style.width = '100%';
+                    newBlock.style.height = 'fit-content';
+                    newBlock.style.backgroundColor = 'transparent';
 
-    if (header) {
-        const newBlock = document.createElement('div');
-        newBlock.id = 'graph';
-        newBlock.style.width = '100%';
-        newBlock.style.height = '400px';
-        newBlock.style.backgroundColor = 'red';
+                    // Добавляем iframe с графиком
+                    newBlock.innerHTML = `
+                        <iframe style="aspect-ratio: 1200 / 630; width:100%; height:fit-content;" 
+                            src="https://www.coindesk.com/embedded-chart/C7NCKfdWrfbfn" 
+                            width="100%" frameborder="0" scrolling="no"></iframe>
+                    `;
 
-        header.parentNode.insertBefore(newBlock, header.nextSibling);
-    }
-}
+                    header.parentNode.insertBefore(newBlock, header.nextSibling);
+                }
+            }
+        }
+    }, 2000);
+});
